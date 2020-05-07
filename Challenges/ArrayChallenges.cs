e5a26d6bfbd512f891a8e1b86854db3a52e48998
Marie Antons <chefantons@gmail.com>
2020-05-06 20:25:56 -0500

    so many things

--- Challenges/ArrayChallenges.cs
+++ Challenges/ArrayChallenges.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Challenges
@@ -49,6 +50,44 @@ namespace Challenges
             //We need to return the shifted array
             return shiftedArray;
 
+        }
+        public static int BinarySearch(int[] array, int searchKey)
+        {
+            int midpoint = 0;
+            int arrayStart = 0;
+            int arrayEnd = array.Length - 1;
+            
+
+            // while loop check if start and end are same
+            while( arrayStart <= arrayEnd)
+            {
+                midpoint = (arrayStart + arrayEnd) / 2;
+                {
+                    if(array[midpoint] < searchKey)
+                    {
+                        arrayStart = midpoint + 1;
+                        Console.WriteLine($"{arrayStart}, {midpoint}, {arrayEnd}");
+                    }
+                    else if (array[midpoint] > searchKey)
+                    {
+
+                        arrayEnd = midpoint - 1;
+                        Console.WriteLine($"{arrayStart}, {midpoint}, {arrayEnd}");
+                    }
+                    else
+                    {
+                        //if midppoint matches key
+                        return midpoint;
+                    }
+                }
+            }
+            //check if key wasnt found on last loop
+            if(array[midpoint] == searchKey)
+            {
+                return midpoint;
+            }
+            // return -1 if not foung
+            return -1;
         }
 
     }
--- ChallengesTest/BinarySearchTests.cs
+++ ChallengesTest/BinarySearchTests.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Challenges;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace ChallengesTest
 {
-    class BinarySearchTests
+    public class BinarySearchTests
     {
+       [Fact]
+       public static void ThereIsAnIndex()
+        {
+            int[] array = { 1, 2, 3, 13, 122, 900};
+            int searchKey = 3;
+            int result = ArrayChallenges.BinarySearch(array, searchKey);
+            Assert.Equal(2, result);
+        }
+        [Fact]
+        public static void KeyNotFound()
+        {
+            int[] array = { 1, 2, 3, 13, 122, 900 };
+            int searchKey = 77;
+            int result = ArrayChallenges.BinarySearch(array, searchKey);
+            Assert.Equal(-1, result);
+        }
+
+
+
+
+
+
+
+
+
+
     }
+
+  
 }
