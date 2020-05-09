ae7addf18b6a96d981cc15d881bedd0b249fbd49
Marie Antons <chefantons@gmail.com>
2020-05-08 22:31:24 -0500

    added second test, and method after failing

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -14,21 +14,35 @@ namespace DataStructures
             public int Value { get; set; }
             public Node Next { get; set; }
 
+
+            //Constructor
             public Node(int value)
             {
                 this.Value = value;
                 this.Next = null;
             }
         }
-   
-        
+
+        /*                              INSERTION
+         - Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+         */
+         public void Insert(int value)
+        {
+            
+                Node newNode = new Node(value);
+                 newNode.Next = Head;
+                 Head = newNode;
+            
+        }
 
 
 
 
 
 
-     
+
+
+
 
         public override string ToString()
         {
--- DataStructuresTests/LinkedListTests.cs
+++ DataStructuresTests/LinkedListTests.cs
@@ -8,13 +8,13 @@ namespace DataStructuresTests
     public class LinkedListTests
 
     /*                  Tests needed: 
-             1. Can successfully instantiate an empty linked list
-             2. Can properly insert into the linked list
-             3. The head property will properly point to the first node in the linked list
-             4. Can properly insert multiple nodes into the linked list
-             5. Will return true when finding a value within the linked list that exists
-             6. Will return false when searching for a value in the linked list that does not exist
-             7. Can properly return a collection of all the values that exist in the linked list
+            [x] 1. Can successfully instantiate an empty linked list
+            [] 2. Can properly insert into the linked list
+            [] 3. The head property will properly point to the first node in the linked list
+            [] 4. Can properly insert multiple nodes into the linked list
+            [] 5. Will return true when finding a value within the linked list that exists
+            [] 6. Will return false when searching for a value in the linked list that does not exist
+            [] 7. Can properly return a collection of all the values that exist in the linked list
 
      */
     {
@@ -25,10 +25,25 @@ namespace DataStructuresTests
 
 
             // Act
-            DataStructures.LinkedList testLinkedList = new LinkedList();
+            LinkedList testLinkedList = new LinkedList();
 
             //Assert
             Assert.NotNull(testLinkedList);
+        }
+
+        [Fact]
+        public void LinkedListInsert()
+        {
+            //Arrange
+            LinkedList insertToLinkedList = new LinkedList();
+
+            //Act
+            insertToLinkedList.Insert(19);
+
+            //Assert
+            Assert.Equal(19, insertToLinkedList.Head.Value);
+
+
         }
     }
 }
