0a7c98d6a8f59935a182f7f0b2da9c4efb440693
Marie Antons <chefantons@gmail.com>
2020-05-08 21:58:14 -0500

    added first test, after failing

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+
+        public Node Head = null;
+  
+        public class Node
+        {
+
+            public int Value { get; set; }
+            public Node Next { get; set; }
+
+            public Node(int value)
+            {
+                this.Value = value;
+                this.Next = null;
+            }
+        }
+   
+        
+
+
+
+
+
+
+     
+
+        public override string ToString()
+        {
+
+            // Traverse and build string
+            return "";
+        }
+    }
+}
---
+++ DataStructuresTests/LinkedListTests.cs
@@ -0,0 +1,34 @@
+using DataStructures;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DataStructuresTests
+{
+    public class LinkedListTests
+
+    /*                  Tests needed: 
+             1. Can successfully instantiate an empty linked list
+             2. Can properly insert into the linked list
+             3. The head property will properly point to the first node in the linked list
+             4. Can properly insert multiple nodes into the linked list
+             5. Will return true when finding a value within the linked list that exists
+             6. Will return false when searching for a value in the linked list that does not exist
+             7. Can properly return a collection of all the values that exist in the linked list
+
+     */
+    {
+        [Fact]
+        public void CreationOfEmptyList()
+        {
+            //Arrange
+
+
+            // Act
+            DataStructures.LinkedList testLinkedList = new LinkedList();
+
+            //Assert
+            Assert.NotNull(testLinkedList);
+        }
+    }
+}
