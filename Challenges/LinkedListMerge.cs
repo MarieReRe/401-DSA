4d90e7c23b992a5d7effa6a86181885be68fe1d6
Marie Antons <chefantons@gmail.com>
2020-05-13 23:33:09 -0500

    Added merge list fuction, not working

--- Challenges/LinkedListMerge.cs
+++ Challenges/LinkedListMerge.cs
@@ -1,9 +1,5 @@
-﻿using DataStructures;
-using System;
-using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Transactions;
+﻿using static DataStructures.LinkedList;
+using DataStructures;
 
 namespace Challenges
 {
@@ -11,10 +7,21 @@ namespace Challenges
     {
         public static LinkedList Merge(LinkedList firstList, LinkedList secondList)
         {
-            
-
             // set head to current
+            Node firstCurrent = firstList.Head;
+            Node secondCurrent = secondList.Head;
+
+            //set up next
+            
+            
            // Head = current;
+           while(firstCurrent != null && secondCurrent != null)
+            {
+               // Node firstNext = firstCurrent.Next;
+               // Node secondNext = secondCurrent.Next;
+
+
+            }
 
 
             //set up our
