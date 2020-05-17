02a6b1edddc474b00df62ac0c1da25bd72e0d565
Marie Antons <chefantons@gmail.com>
2020-05-17 15:11:36 -0500

    First test for Queue Failed RED

--- DataStructures/Queues.cs
+++ DataStructures/Queues.cs
@@ -4,7 +4,43 @@ using System.Text;
 
 namespace DataStructures
 {
-    class Queues
+    public class Queues<T>
     {
+        private Node<T> Front = null;
+        private Node<T> Rear = null;
+       
+
+        public class Node<T>
+        {
+            public Node<T> Front { get; set; }
+            public Node<T> Rear { get; set; }
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+
+            //initial constructor that adds a link that points to null
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+        }
+
+
+        /*--------------------------------------------- QUEUE START---------
+        -------------------------------------------------------------------*/
+        public void Enqueue(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            if (Rear != null)
+            {
+                newNode.Value = value;
+                Rear.Next = newNode;
+                newNode = Rear;
+            }
+            else
+            {
+                Rear = newNode;
+            }
+        }
     }
 }
--- DataStructuresTests/QueueTesting.cs
+++ DataStructuresTests/QueueTesting.cs
@@ -5,9 +5,33 @@ using Xunit;
 
 namespace DataStructuresTests
 {
-   /* class QueueTesting
+
+
+    /*----------------------------------QUEUE TESTING
+                   [] 1. Can successfully enqueue into a queue
+                   [] 2. Can successfully enqueue multiple values into a queue
+                   [] 3. Can successfully dequeue out of a queue the expected value
+                   [] 4. Can successfully peek into a queue, seeing the expected value
+                   [] 5. Can successfully empty a queue after multiple dequeues
+                   [] 6. Can successfully instantiate an empty queue
+                   [] 7. Calling dequeue or peek on empty queue raises exception x2 DEQUEUE & PEEK
+
+
+           -----------------------------------------*/
+
+
+
+    public class QueueTesting
     {
         [Fact]
+        public void CanEnqueueToQueue()
+        {
+            Queue<int> newQueue = new Queue<int>();
+            newQueue.Enqueue(1);
+
+            Assert.Contains(5, newQueue);
+
+        }
     }
-    */
+   
 }
