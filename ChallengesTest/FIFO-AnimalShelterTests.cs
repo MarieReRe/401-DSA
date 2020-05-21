65acdf3ef343ca1e46ac5917cfbe56a035babb78
Marie Antons <chefantons@gmail.com>
2020-05-20 19:17:38 -0500

    MB creation

---
+++ ChallengesTest/FIFO-AnimalShelterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenges;
+using Xunit;
+
+namespace ChallengesTest
+{
+    public class FifoAnimalShelterTests
+    {
+        [Fact]
+        public void EnqueueCat()
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            FifoAnimalShelter shelter = new FifoAnimalShelter();
+
+            shelter.ArriveAtShelter(dog);
+            shelter.ArriveAtShelter(cat);
+
+            //act: generic constraint specifies a type that matches the criteria. 
+           Cat result = shelter.TakeHome<Cat>();
+
+
+            //Assert
+            Assert.Equal(cat, result);
+        }
+    }
+}
