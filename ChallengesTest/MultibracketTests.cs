ef1d4025b583ad3ed41d7a326c75f0f37778e98b
Marie Antons <chefantons@gmail.com>
2020-05-20 19:45:33 -0500

    Theory testing Passes GREEN

---
+++ ChallengesTest/MultibracketTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenges;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace ChallengesTest
+{
+    public class MultibracketTests
+    {
+        [Theory]
+        [InlineData("<(!--{Hello!})>", true)]
+        [InlineData("<>", true)]
+        [InlineData("()})>", false)]
+        [InlineData("!)", false)]
+        [InlineData("(", false)]
+        [InlineData("<(!-d-{(d)(d)()()()h}h)h>", true)]
+        [InlineData("{}", true)]
+        public void MatchedBracket(string input, bool expected)
+        {
+          
+            bool actual = MultiBracket.IsMatched(input);
+
+            Assert.Equal(expected, actual);
+
+
+
+
+        }
+
+       
+
+
+
+    }
+}
