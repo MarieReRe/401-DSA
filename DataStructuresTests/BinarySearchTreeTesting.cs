a07704b8b0045074b01e3ebe5fe4b7259c4d3bae
Marie Antons <chefantons@gmail.com>
2020-05-24 18:34:20 -0500

    PostOrder test part 5 complete GREEN

--- DataStructuresTests/BinarySearchTreeTesting.cs
+++ DataStructuresTests/BinarySearchTreeTesting.cs
@@ -143,8 +143,8 @@ namespace DataStructuresTests
             Assert.Equal(expected[4], newTree.Root.Value);
             Assert.Equal(expected[0], newTree.Root.Left.Value);
             Assert.Equal(expected[1], newTree.Root.Left.Right.Value);
-           Assert.Equal(expected[2], newTree.Root.Right.Value);
-          //  Assert.Equal(expected[4], newTree.Root.Right.Right.Value);
+            Assert.Equal(expected[2], newTree.Root.Right.Value);
+            Assert.Equal(expected[3], newTree.Root.Right.Right.Value);
 
 
 
