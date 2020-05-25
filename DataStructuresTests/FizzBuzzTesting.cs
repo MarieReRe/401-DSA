7c7d130bb22b89b7f8116c4da28231c4c9158d85
Marie Antons <chefantons@gmail.com>
2020-05-25 18:52:52 -0500

    third test complete and passing GREEN

--- DataStructuresTests/FizzBuzzTesting.cs
+++ DataStructuresTests/FizzBuzzTesting.cs
@@ -6,6 +6,11 @@ using Xunit;
 
 namespace DataStructuresTests
 {
+
+    /*-------------------------------------------------FIZZBUZZ TESTING: 
+                    [] 1. Instantiate empty tree 
+                    [] 2. Create tree starting with an int
+-------------------------------------------------------------------------------------------------------------------------------------*/
     public class FizzBuzzTesting
     {
         [Fact]
@@ -17,8 +22,14 @@ namespace DataStructuresTests
         [Fact]
         public void CreateTreeWithIntAsRoot()
         {
-            Tree<object> newTree = new Tree<object>(70);
-            Assert.Equal(70, newTree.Root.Value);
+            Tree<object> newTree = new Tree<object>(1);
+            Assert.Equal(1, newTree.Root.Value);
+        }
+        [Fact]
+        public void CanInstantiateTreeWithString()
+        {
+            Tree<object> tree = new Tree<object>("Marie");
+            Assert.Equal("Marie", tree.Root.Value);
         }
 
     }
