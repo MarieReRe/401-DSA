8e5bbe096d0168f8609d3df99c5a4b80ac8e2481
Marie Antons <chefantons@gmail.com>
2020-06-08 21:39:56 -0500

    insertion sorting

--- Challenges/Sorting/InsertionSorting.cs
+++ Challenges/Sorting/InsertionSorting.cs
@@ -7,16 +7,16 @@ namespace Challenges.Sorting
     public class InsertionSorting
     {
           public static int[] InsertionSort(int [] arr)
-        {
-            for (int i =0; i< arr.Length; i++)
+          {
+            for (int i = 0; i < arr.Length; i++)
             {
                 int j = i - 1;
                 int temp = arr[i];
 
-               while (j>=0 && temp<arr[j])
+               while (j >= 0 && temp < arr [j] )
                 {
                    
-                    arr[j+1] = arr[j];
+                    arr[j + 1] = arr[j];
                     j = j - 1;
                     arr[j + 1] = temp;
     
@@ -26,7 +26,7 @@ namespace Challenges.Sorting
                 }
             return arr;
               
-            }
-        }
+          }
+        
     }
 }
