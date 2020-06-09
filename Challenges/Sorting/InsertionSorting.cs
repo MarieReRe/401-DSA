e61ddb1f81cec711ec9ae9d16daad7f9a70e7ace
Marie Antons <chefantons@gmail.com>
2020-06-08 21:49:51 -0500

    insertion sorting

--- Challenges/Sorting/InsertionSorting.cs
+++ Challenges/Sorting/InsertionSorting.cs
@@ -6,17 +6,28 @@ namespace Challenges.Sorting
 {
     public class InsertionSorting
     {
+        
           public static int[] InsertionSort(int [] arr)
           {
+             // Traverse through length of array, starting with the element at index 0.
             for (int i = 0; i < arr.Length; i++)
             {
                 int j = i - 1;
                 int temp = arr[i];
 
+                
+                 // Iterate through sorted items.
+                 // If the current unsorted item is smaller than the item to its left,
+                 // move the current item back one position in the array.
+                 // This loop will never run for the very first unsorted item at index 0.
+
                while (j >= 0 && temp < arr [j] )
                 {
                    
                     arr[j + 1] = arr[j];
+
+                    
+                //This while loop shifts items to the right in the sorted subset of the array
                     j = j - 1;
                     arr[j + 1] = temp;
     
