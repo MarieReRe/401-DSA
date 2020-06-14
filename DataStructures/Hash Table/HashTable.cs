8245cee3601a9c2df35be26abe517ded1dcbe511
Marie Antons <chefantons@gmail.com>
2020-06-13 20:46:38 -0500

    added bucket and hashNode

--- DataStructures/Hash Table/HashTable.cs
+++ DataStructures/Hash Table/HashTable.cs
@@ -21,6 +21,17 @@ namespace DataStructures.Hash_Table
             }
 
         }
-       
+        //Set up how many buckets are in the HT
+        public int Buckets { get; set; }
+
+        //This sets upthe Node array that will store multiple key/value pairs. 
+        public Node[] HashNode { get; set; }
+
+        //Next, we set up the HT Constructor. This will set up the quantity for our buckets and set up the same amount of node arrays to match the buckets!
+        public HashTable(int buckets)
+        {
+            Buckets = buckets;
+            HashNode = new Node[buckets];
+        }
     }
 } 
