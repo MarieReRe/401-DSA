202bfab2bab46d6c13380da655db29bc8e7b4dc2
Marie Antons <chefantons@gmail.com>
2020-06-13 21:39:02 -0500

    one test passing

--- DataStructures/Hash Table/HashTable.cs
+++ DataStructures/Hash Table/HashTable.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace DataStructures.Hash_Table
 {
+
+    // Key value look-up
+    //we want to store the date in an array
     public class HashTable
     {
         // First We need to set up our Hash Table. Our Hash table will use a node
@@ -21,17 +24,80 @@ namespace DataStructures.Hash_Table
             }
 
         }
+
+        //Set Up the Hash
+        public int Hash(string key)
+        {
+            int index;
+            byte[] bytesTotal = Encoding.ASCII.GetBytes(key);
+            int letterNumber = 0;
+            for (int i = 0; i < bytesTotal.Length; i++)
+            {
+                letterNumber += bytesTotal[i];
+            }
+            index = letterNumber * 1024 % 1147 % Buckets;
+            return index;
+        }
         //Set up how many buckets are in the HT
         public int Buckets { get; set; }
 
         //This sets upthe Node array that will store multiple key/value pairs. 
-        public Node[] HashNode { get; set; }
+        public Node[] HashTableNode { get; set; }
 
         //Next, we set up the HT Constructor. This will set up the quantity for our buckets and set up the same amount of node arrays to match the buckets!
         public HashTable(int buckets)
         {
             Buckets = buckets;
-            HashNode = new Node[buckets];
+            HashTableNode = new Node[buckets];
+        }
+
+        //Adds an element with the specified key and value into the Hashtable.
+        public void Add(string key, string value)
+        {
+            int index = Hash(key);
+            if (HashTableNode[index] == null)
+            {
+                Node newNode = new Node(key, value);
+                HashTableNode[index] = newNode;
+            }
+            else
+            {
+                Node newNode = new Node(key, value);
+                Node current = HashTableNode[index];
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        // Get
+
+
+
+        //Contains
+        public bool Contains(string key)
+        {
+            int index = Hash(key);
+            if (HashTableNode[index] == null)
+            {
+                return false;
+            }
+            else
+            {
+                Node current = HashTableNode[index];
+                if (HashTableNode[index].Key == key) return true;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                    if (current.Key == key)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
     }
 } 
---
+++ DataStructuresTests/HashTableTesting.cs
@@ -0,0 +1,21 @@
+﻿using DataStructures.Hash_Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructuresTests
+{
+    public class HashTableTesting
+    {
+        [Fact]
+        public void AddOneValue()
+        {
+            HashTable testHT = new HashTable(15);
+           testHT.Add("Vanilla Bean", "Lemon Frozen Yogurt");
+            Assert.True(testHT.Contains("Vanilla Bean"));
+        }
+    }
+
+
+}
