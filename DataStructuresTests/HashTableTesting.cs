79f52da936855dfb8f5a20fcf5aeab7275c1f8f4
Marie Antons <chefantons@gmail.com>
2020-06-14 09:49:30 -0500

    testing testing

--- DataStructuresTests/HashTableTesting.cs
+++ DataStructuresTests/HashTableTesting.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructures.Hash_Table;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -12,8 +13,52 @@ namespace DataStructuresTests
         public void AddOneValue()
         {
             HashTable testHT = new HashTable(15);
-           testHT.Add("Vanilla Bean", "Lemon Frozen Yogurt");
-            Assert.True(testHT.Contains("Vanilla Bean"));
+           testHT.Add("Ice Cream","Vanilla Bean");
+            Assert.True(testHT.Contains("Ice Cream"));
+        }
+
+
+        [Fact]
+        public void CollisionHandling()
+        {
+            Hashtable testHT = new Hashtable(1);
+
+            testHT.Add("Ice Cream", "chocolate");
+            testHT.Add("Topping", "sprinkles");
+            Assert.True(testHT.Contains("Ice Cream"));
+            Assert.True(testHT.Contains("Topping"));
+        }
+        [Fact]
+        public void ContainsNoKeys()
+        {
+            Hashtable testHT = new Hashtable(5);
+            Assert.False(testHT.Contains("Marie"));
+        }
+
+        [Fact]
+        public void CanAddMultipleKeyValues()
+        {
+            string[] productID = { "1001", "2001", "1002", "2002", "3001", "4002", "5009" };
+            string[] product = { "t-shirt", "dad cap", "cooler", "Ice Cream Cookbook", "pencil set", "note pad", "scooper" };
+
+            HashTable hashtable = new HashTable(productID.Length);
+            for (int i = 0; i < productID.Length; i++)
+            {
+                hashtable.Add(productID[i], product[i]);
+                Assert.True(hashtable.Contains(productID[i]));
+            }
+        }
+        [Fact]
+        public void ContainsMultiple()
+        {
+            Hashtable testHT = new Hashtable(5);
+            testHT.Add("Spaghetti", "1");
+            testHT.Add("Ravioli", "3");
+            testHT.Add("strozzapreti", "11");
+            Assert.True(testHT.Contains("Spaghetti"));
+            Assert.True(testHT.Contains("Ravioli"));
+            Assert.True(testHT.Contains("strozzapreti"));
+
         }
     }
 
