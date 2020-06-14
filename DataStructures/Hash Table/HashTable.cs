2cd963400fd865f7061d0bf29279045e9627042d
Marie Antons <chefantons@gmail.com>
2020-06-13 20:46:17 -0500

    added node

---
+++ DataStructures/Hash Table/HashTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Hash_Table
+{
+    public class HashTable
+    {
+        // First We need to set up our Hash Table. Our Hash table will use a node
+        public class Node
+        {
+
+            public string Key { get; set; }
+            public string Value { get; set; }
+            public Node Next { get; set; }
+            public Node(string key, string value)
+            {
+                Key = key;
+                Value = value;
+                Next = null;
+            }
+
+        }
+       
+    }
+} 
