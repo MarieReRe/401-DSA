e0d5bbfecd9f61db8b0c3648de3fe04f1de96c65
Marie Antons <chefantons@gmail.com>
2020-06-20 18:40:35 -0500

    file structure and one failing test...RED

---
+++ DataStructures/GraphImplementation/Graph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.GraphImplementation
+{
+   public class Graph
+    {
+        //Add Node
+        //Add Edge
+        //GetNodes
+        //GetNeighbors
+        //Size
+    }
+}
---
+++ DataStructuresTests/GraphTesting.cs
@@ -0,0 +1,33 @@
+﻿using DataStructures.GraphImplementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructuresTests
+{
+   public class GraphTesting
+    {
+
+        /* -------------------------------------------- GRAPH TESTING--------------------------------------------------------------
+                            [] 1. Node can be successfully added to the graph
+                            [] 2. An edge can be successfully added to the graph
+                            [] 3. A collection of all nodes can be properly retrieved from the graph
+                            [] 4. All appropriate neighbors can be retrieved from the graph
+                            [] 5. Neighbors are returned with the weight between nodes included
+                            [] 6. The proper size is returned, representing the number of nodes in the graph
+                            [] 7. A graph with only one node and edge can be properly returned
+                            [] 8. An empty graph properly returns null
+         
+      -----------------------------------------------------------------------------------------------------------------------   */
+        [Fact]
+        public void EmptyGraphReturnsNull()
+        {
+            //arrange
+            Graph newGraph = new Graph();
+            //Act
+            //Assert
+            Assert.True(null);
+        }
+    }
+}
