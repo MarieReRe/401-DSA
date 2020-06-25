43ae4f16e6ecc823eb74eca54539ffc41bacb7cc
Marie Antons <chefantons@gmail.com>
2020-06-24 19:48:22 -0500

    DFT

---
+++ Challenges/DepthFirst/DFS.cs
@@ -0,0 +1,43 @@
+﻿using DataStructures.GraphImplementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.DepthFirst
+{
+    public class DFS
+    {
+        public static List<int> DepthFirst(Graph<int> graph, int root)
+        {
+
+
+            if (graph is null) return null;
+            if (graph.Size() < 1) return null;
+            if (graph.GetNeighbors(root) is null) return null;
+
+            Dictionary<int, bool> visited = new Dictionary<int, bool>();
+            Stack<int> stack = new Stack<int>();
+            List<int> result = new List<int>();
+
+            stack.Push(root);
+            while (stack.TryPop(out int node))
+            {
+                if (!visited.ContainsKey(node))
+                {
+                    result.Add(node);
+                    visited.Add(node, true);
+
+                    if (graph.GetNeighbors(node).Keys.Count < 1) continue;
+                    foreach (int neighbor in graph.GetNeighbors(node).Keys)
+                    {
+                        if (!visited.ContainsKey(neighbor))
+                            stack.Push(neighbor);
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ ChallengesTest/DFTTesting.cs
@@ -0,0 +1,21 @@
+﻿using Challenges.DepthFirst;
+using DataStructures.GraphImplementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChallengesTest
+{
+   public class DFTTesting
+    {
+        [Fact]
+        public void DepthFirstOnEmptyGraphReturnsNull()
+        {
+            Graph<int> graph = new Graph<int>();
+            Assert.Null(DFS.DepthFirst(graph, 1));
+        }
+
+
+    }
+}
